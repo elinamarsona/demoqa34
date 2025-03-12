016d42229b9a0608c738b47d125ec0fd6725a3b7
Elina Kulikova <elinamarsona@gmail.com>
2025-03-12 23:01:30 +0300

    mode name SolutionsEnterprizeHoverTest class

--- src/test/java/tests/SolutionsEnterprizeHoverTest.java
+++ src/test/java/tests/SolutionsEnterprizeHoverTest.java
@@ -1,14 +1,11 @@
 package tests;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.files.DownloadActions.click;
 
-public class SolutionsEnterprizeHover {
+public class SolutionsEnterprizeHoverTest {
 
     @Test
     void SolutionsEnterprizeHoverTest(){
