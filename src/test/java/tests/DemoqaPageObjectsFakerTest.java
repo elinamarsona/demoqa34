be532f55c0a412d01390d15e80fbc0b7d9f3c040
Elina Kulikova <elinamarsona@gmail.com>
2025-04-21 15:33:50 +0300

    add allure

--- src/test/java/tests/DemoqaPageObjectsFakerTest.java
+++ src/test/java/tests/DemoqaPageObjectsFakerTest.java
@@ -1,19 +1,29 @@
 package tests;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.github.javafaker.Faker;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 import utils.RandomUtils;
 
 import java.util.Locale;
 
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+
+@DisplayName("Проверка регистрации на странице demoqa")
 public class DemoqaPageObjectsFakerTest extends TestBase {
 
     RegistrationPage registrationPage = new RegistrationPage();
     RandomUtils randomUtils = new RandomUtils();
 
+    @DisplayName("Успешная регистрация при заполнении всех полей")
     @Test
     void successRegistrationTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        step("Открыть страницу и заполнить все поля", () -> {
         registrationPage.openPage()
                 .setFirstName(randomUtils.firstName)
                 .setLastName(randomUtils.lastName)
@@ -25,9 +35,12 @@ public class DemoqaPageObjectsFakerTest extends TestBase {
                 .setHobbies(randomUtils.hobbies)
                 .setPicture(randomUtils.uploadImage)
                 .setAddress(randomUtils.currentAddress)
-                .setStateAndCity(randomUtils.state, randomUtils.city)
-                .submitForm();
-
+                .setStateAndCity(randomUtils.state, randomUtils.city);
+        });
+        step("Нажать кнопку Submit", () -> {
+            registrationPage.submitForm();
+        });
+        step("Проверить результат регистрации", () -> {
         registrationPage.checkResult("Student Name", randomUtils.firstName + " " + randomUtils.lastName)
                 .checkResult("Student Email", randomUtils.userEmail)
                 .checkResult("Gender", randomUtils.gender)
@@ -38,40 +51,62 @@ public class DemoqaPageObjectsFakerTest extends TestBase {
                 .checkResult("Picture", "2.jpg")
                 .checkResult("Address", randomUtils.currentAddress)
                 .checkResult("State and City", randomUtils.state + " " + randomUtils.city);
+        });
     }
 
+    @DisplayName("Успешная регистрация при заполнении только обязательных полей")
     @Test
     void successRegistrationMinimalInputTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        step("Открыть страницу и заполнить обязательные поля", () -> {
         registrationPage.openPage()
                 .removeBanner()
                 .setFirstName(randomUtils.firstName)
                 .setLastName(randomUtils.lastName)
                 .setGender(randomUtils.gender)
-                .setUserNumber(randomUtils.userNumber)
-                .submitForm();
-
+                .setUserNumber(randomUtils.userNumber);
+        });
+        step("Нажать кнопку Submit", () -> {
+            registrationPage.submitForm();
+        });
+        step("Проверить результат регистрации", () -> {
         registrationPage.checkResult("Student Name", randomUtils.firstName + " " + randomUtils.lastName)
                  .checkResult("Gender", randomUtils.gender)
                 .checkResult("Mobile", randomUtils.userNumber);
+        });
     }
 
+    @DisplayName("Несостоявшаяся регистрация при нажатии только кнопки Submit")
     @Test
     void negativeRegistrationTest1() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        step("Открыть страницу", () -> {
         registrationPage.openPage()
-                .removeBanner()
-                .submitForm();
-
+                .removeBanner();
+        });
+        step("Нажать кнопку Submit", () -> {
+            registrationPage.submitForm();
+        });
+        step("Проверить результат регистрации", () -> {
         registrationPage.checkInvalidInput();
+        });
     }
 
+    @DisplayName("Несостоявшаяся регистрация при заполнении одного обязательного поля и нажатии кнопки Submit")
     @Test
     void negativeRegistrationTest2() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        step("Открыть страницу и заполнить одно обязательное поле", () -> {
         registrationPage.openPage()
                 .removeBanner()
-                .setFirstName(randomUtils.firstName)
-                .submitForm();
-
-        registrationPage.checkInvalidInput();
+                .setFirstName(randomUtils.firstName);
+        });
+        step("Нажать кнопку Submit", () -> {
+            registrationPage.submitForm();
+        });
+        step("Проверить результат регистрации", () -> {
+            registrationPage.checkInvalidInput();
+        });
     }
 
 }
